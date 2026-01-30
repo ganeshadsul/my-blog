f70a2a3b281bf453b959b75e6cfe15f97d7d4972
Ganesh Adsul <ganeshadsul76@gmail.com>
2026-01-31 00:59:19 +0530

    Configuration added for different environments

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
 
+const env = process.env.NODE_ENV || 'development';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${env}`,
+      validationSchema: Joi.object({
+        MONGO_URL: Joi.string().required(),
+        BACKEND_PORT: Joi.number().default(3000),
+        NODE_ENV: Joi.string()
+          .valid('development', 'qa', 'staging', 'production')
+          .default('development'),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,12 +6,14 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+  const port = configService.get<number>('BACKEND_PORT') || 3000;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
